10181854358ed001bc3ae935676b6606bf0682d4
Michael Cuneo <michaelcuneo75@gmail.com>
2025-06-16 14:08:43 +1000

    Added Export by Date

--- export_gradebook_history.js
+++ export_gradebook_history.js
@@ -16,16 +16,17 @@
 	const courseId = getCourseId();
 
 	const uniqueLinkId = 'mw_export_all';
+	const uniqueLinkIdByDate = 'mw_export_by_date';
 	const uniqueLinkIdByStudent = 'mw_export_by_student';
 	const uniqueDialogId = 'mw_export_gradebook_dialog';
+	const uniqueDateDialogId = 'mw_export_gradebook_date_dialog';
 
 	const addButtons = (buttonText, buttonIcon) => {
 		var parent;
 
-		if (document.getElementById(uniqueLinkId) || document.getElementById(uniqueLinkIdByStudent))
-        {
+		if (document.getElementById(uniqueLinkId) || document.getElementById(uniqueLinkIdByStudent) || document.getElementById(uniqueLinkIdByDate)) {
 			return;
-        }
+		}
 
 		parent = document.querySelector('#wrapper > div.ic-app-nav-toggle-and-crumbs.no-print');
 
@@ -48,6 +49,22 @@
 		anchorExportButton.appendChild(document.createTextNode(` ${buttonText}`));
 		parent.appendChild(anchorExportButton);
 
+		var anchorExportHistoryDateButton = document.createElement('a');
+
+		anchorExportHistoryDateButton.classList.add('btn');
+		anchorExportHistoryDateButton.id = uniqueLinkIdByDate;
+		anchorExportHistoryDateButton.addEventListener('click', () => {
+			openDateDialog();
+		})
+		anchorExportHistoryDateButton.style.margin = '5px';
+
+		var icon3 = document.createElement('i');
+
+		icon3.classList.add(buttonIcon);
+		anchorExportHistoryDateButton.appendChild(icon3);
+		anchorExportHistoryDateButton.appendChild(document.createTextNode(' Export by Date'));
+		parent.appendChild(anchorExportHistoryDateButton);
+
 		var anchorExportHistoryButton = document.createElement('a');
 
 		anchorExportHistoryButton.classList.add('btn');
@@ -66,6 +83,35 @@
 
 		return;
 	};
+
+	// Create the dialog for exporting by student
+	function createDateDialog() {
+		var el = document.querySelector(`#${uniqueDateDialogId}`);
+
+		if (!el) {
+			el = document.createElement('div');
+			el.id = uniqueDateDialogId;
+			el.classList.add('ic-Form-control');
+			var label = document.createElement('label');
+			label.htmlFor = 'mw_export_gradebook_date_text';
+			label.textContent = 'Date';
+			label.classList.add('ic-Label');
+			el.appendChild(label);
+			var datePicker = document.createElement('input');
+			datePicker.id = 'mw_export_gradebook_date_text';
+			datePicker.type = 'date';
+			datePicker.classList.add('ic-Input');
+			datePicker.placeholder = '';
+			el.appendChild(datePicker);
+			var msg = document.createElement('div');
+			msg.id = 'mw_export_gradebook_msg';
+			msg.classList.add('ic-flash-warning');
+			msg.style.display = 'none';
+			el.appendChild(msg);
+			var parent = document.querySelector('body');
+			parent.appendChild(el);
+		}
+	}
 
 	// Create the dialog for exporting by student
 	function createDialog() {
@@ -93,6 +139,49 @@
 			el.appendChild(msg);
 			var parent = document.querySelector('body');
 			parent.appendChild(el);
+		}
+	}
+
+	function openDateDialog() {
+		if (!checkId()) {
+			alert('Could not determine course. Aborting.');
+
+			return;
+		}
+
+		try {
+			createDateDialog();
+
+			// Disabled eslint rule for jQuery usage
+			// eslint-disable-next-line no-undef
+			$(`#${uniqueDateDialogId}`).dialog({
+				title: 'Export Student Gradebook History Since Date',
+				autoOpen: false,
+				buttons: [
+					{
+						text: 'Cancel',
+						click: closeDateDialog
+					},
+					{
+						text: 'Export',
+						click: checkDateDialog,
+						class: 'Button Button--primary'
+					}
+				],
+
+				modal: true,
+				height: 'auto',
+				width: '30%'
+			});
+			// Disabled eslint rule for jQuery usage
+			// eslint-disable-next-line no-undef
+			if (!$(`#${uniqueDateDialogId}`).dialog('isOpen')) {
+				// Disabled eslint rule for jQuery usage
+				// eslint-disable-next-line no-undef
+				$(`#${uniqueDateDialogId}`).dialog('open');
+			}
+		} catch (e) {
+			console.log(e);
 		}
 	}
 
@@ -151,6 +240,30 @@
 		}
 	}
 
+	function closeDateDialog() {
+		// Disabled eslint rule for jQuery usage
+		// eslint-disable-next-line no-undef
+		$(`#${uniqueDialogId}`).dialog('close');
+		var el = document.getElementById('mw_export_gradebook_date_text');
+		if (el) {
+			el.value = '';
+		}
+	}
+
+	async function checkDateDialog() {
+		var inputDate = document.getElementById('mw_export_gradebook_date_text').value.trim();
+		console.log(inputDate);
+
+		if (!inputDate) {
+			alert('Enter a date!');
+			return false;
+		}
+
+		closeDialog();
+		processRequestByDate(inputDate);
+
+	}
+
 	async function checkDialog() {
 		var inputUserSISId = document.getElementById('mw_export_gradebook_text').value.trim();
 
@@ -165,6 +278,51 @@
 
 		closeDialog();
 		processRequestByStudent(user);
+	}
+
+	async function processRequestByDate(date) {
+		if (!checkId()) {
+			alert('Could not determine course. Aborting.');
+			return;
+		}
+		toggleLoadingSpinner();
+		const gradeChanges = await getGradeChangeLogByDate(date);
+		var csvArr = [];
+		var recordDate;
+
+		if (gradeChanges.length === 0) {
+			toggleLoadingSpinner();
+			alert('No Grade Changes found.');
+			return;
+		}
+
+		const filteredGradeChanges = gradeChanges.map((gradeChange) => {
+
+			recordDate = new Date(gradeChange.created_at);
+			return {
+				date: recordDate.toLocaleString(),
+				student_name: gradeChange.user ? gradeChange.user.name : 'Dropped Record',
+				sis_user_id: gradeChange.user ? gradeChange.user.sis_user_id : 'Dropped Record',
+				student_id: gradeChange.links.student ? gradeChange.links.student : 'Dropped Record',
+				grader: gradeChange.grader ? gradeChange.grader.name : '',
+				artefact: gradeChange.assignment
+					? gradeChange.assignment.name
+					: gradeChange.course_override_grade
+						? 'FINAL GRADE OVERRIDE'
+						: '',
+				grade_before: gradeChange.grade_before ? gradeChange.grade_before : '',
+				grade_after: gradeChange.grade_after ? gradeChange.grade_after : '',
+			};
+		});
+
+		const headers = Object.keys(filteredGradeChanges[0]);
+		csvArr.push(headers, ...filteredGradeChanges.map((gradeChange) => Object.values(gradeChange)));
+
+		const fileName = 'gradebook_history';
+
+		exportToCsv(fileName, csvArr);
+		toggleLoadingSpinner();
+		return;
 	}
 
 	async function processRequestByStudent(user) {
@@ -261,9 +419,9 @@
 	}
 
 	async function getUser(sisUserId) {
-        // https://canvas.newcastle.edu.au/api/v1/courses/${courseId}/users/${sisUserId}
+		// https://canvas.newcastle.edu.au/api/v1/courses/${courseId}/users/${sisUserId}
 		// var url = `${window.location.origin}/api/v1/users/sis_user_id:${sisUserId}`;
-        var url = `${window.location.origin}/api/v1/courses/${courseId}/users/${sisUserId}`;
+		var url = `${window.location.origin}/api/v1/courses/${courseId}/users/${sisUserId}`;
 		var settings = {
 			headers: {
 				'X-CSRFToken': getCsrfToken()
@@ -312,10 +470,9 @@
 				if (!users.map((user) => user.id).some((id) => id === user.id)) users.push(user);
 			});
 			data.linked.assignments.forEach((assignment) => {
-				if (!assignments.map((assignment) => assignment.id).some((id) => id === assignment.id))
-                {
+				if (!assignments.map((assignment) => assignment.id).some((id) => id === assignment.id)) {
 					assignments.push(assignment);
-                }
+				}
 			});
 
 			parsedLinkHeader = parseLinkHeader(response.headers.get('link'));
@@ -366,26 +523,23 @@
 			const data = await response.json();
 
 			gradeChanges = [...gradeChanges, ...data.events];
-            // Get the user because the grade_change url doesn't supply SIS_User_Id
-            for (const user of data.linked.users) {
-                const sisUser = await getUser(user.id);
-                console.log(sisUser);
-                if (sisUser !== false || sisUser !== undefined) {
-                    if (!users.map((user) => user.id).some((id) => id === user.id)) users.push(sisUser);
-                }
-            };
-            console.log('Users:', users);
-            // Old way of getting the users was broken.
+			// Get the user because the grade_change url doesn't supply SIS_User_Id
+			for (const user of data.linked.users) {
+				const sisUser = await getUser(user.id);
+				if (sisUser !== false || sisUser !== undefined) {
+					if (!users.map((user) => user.id).some((id) => id === user.id)) users.push(sisUser);
+				}
+			};
+			// Old way of getting the users was broken.
 			/*
-            data.linked.users.forEach((user) => {
+						data.linked.users.forEach((user) => {
 				if (!users.map((user) => user.id).some((id) => id === user.id)) users.push(sisUser);
 			});
-            */
+						*/
 			data.linked.assignments.forEach((assignment) => {
-				if (!assignments.map((assignment) => assignment.id).some((id) => id === assignment.id))
-                {
+				if (!assignments.map((assignment) => assignment.id).some((id) => id === assignment.id)) {
 					assignments.push(assignment);
-                }
+				}
 			});
 
 			parsedLinkHeader = parseLinkHeader(response.headers.get('link'));
@@ -406,7 +560,89 @@
 				(assignment) => assignment.id == gradeChange.links.assignment
 			);
 		});
-        console.log('Grade Changes', gradeChanges);
+
+		return gradeChanges;
+	}
+
+	async function getGradeChangeLogByDate(date) {
+		var gradeChanges = [];
+		var users = [];
+		var assignments = [];
+		var parsedLinkHeader, url;
+		var dateRangeHit = false;
+
+		url = `${window.location.origin}/api/v1/audit/grade_change/courses/${courseId}?per_page=100`;
+
+		var settings = {
+			//method: 'GET',
+			headers: {
+				'X-CSRFToken': getCsrfToken()
+			}
+		};
+		console.log(`Fetching current grade changes...`);
+		var i = 1;
+		while (url !== null) {
+			const response = await fetch(url, settings);
+
+			if (!response.ok) {
+				return false;
+			}
+
+			const data = await response.json();
+
+			for (const event of data.events) {
+				// console.log(event.created_at);
+				const dateIso = new Date(date).toISOString();
+				if (dateIso <= event.created_at) {
+					console.log('Date for first record: ', dateIso);
+					console.log('Date for entry: ', event.created_at);
+					gradeChanges = [...gradeChanges, event];
+				} else {
+					dateRangeHit = true;
+				}
+			}
+
+			// Get the user because the grade_change url doesn't supply SIS_User_Id
+			for (const user of data.linked.users) {
+				const sisUser = await getUser(user.id);
+				if (sisUser !== false || sisUser !== undefined) {
+					if (!users.map((user) => user.id).some((id) => id === user.id)) users.push(sisUser);
+				}
+			};
+			// Old way of getting the users was broken.
+			/*
+						data.linked.users.forEach((user) => {
+				if (!users.map((user) => user.id).some((id) => id === user.id)) users.push(sisUser);
+			});
+						*/
+			data.linked.assignments.forEach((assignment) => {
+				if (!assignments.map((assignment) => assignment.id).some((id) => id === assignment.id)) {
+					assignments.push(assignment);
+				}
+			});
+
+			parsedLinkHeader = parseLinkHeader(response.headers.get('link'));
+
+			if (dateRangeHit) {
+				url = null;
+			} else if (parsedLinkHeader && parsedLinkHeader.next) {
+				url = parsedLinkHeader.next;
+			} else {
+				url = null
+			}
+			console.log(`Fetched from page ${i}. Grade changes: ${gradeChanges.length}.`);
+			i++;
+		}
+
+		console.log(dateRangeHit);
+
+		gradeChanges.forEach((gradeChange) => {
+			gradeChange.user = users.find((user) => user.id == gradeChange.links.student);
+			gradeChange.grader = users.find((user) => user.id == gradeChange.links.grader);
+			gradeChange.assignment = assignments.find(
+				(assignment) => assignment.id == gradeChange.links.assignment
+			);
+		});
 
 		return gradeChanges;
 	}
